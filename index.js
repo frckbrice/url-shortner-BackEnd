f33e09fa32f93dbe3f8fcb8a47a2063205cf2e0f
frckbrice <maebrie2017@gmail.com>
2023-12-27 19:56:30 +0100

    modification on corsoptions

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://uner.vercel.app/", "*"],
+  origin: "https://uner.vercel.app/",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 //* this key finally "open  my door" since the whole day i struggled to solve the pb of empty body data. this tell express to parse the text/plain to json
@@ -17,15 +17,8 @@ app.use(
   })
 );
 
-//* some options to add for CORES issues
-// app.options("*", (req, res) => {
-//   res.setHeader("Access-Control-Allow-Origin", "https://uner.vercel.app/", "*");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type"); // Add other headers here
-//   res.setHeader("Access-Control-Allow-Methods", ", GET, POST"); // Add other methods here
-//   res.send();
-// });
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use("/api/shorturl", require("./routes/url"));
 
 app.use(express.static(path.join(__dirname + "/public/api")));
