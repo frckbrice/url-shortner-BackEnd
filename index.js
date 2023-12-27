4c97d19917d656bfe79697eddcd372e201ec1fed
frckbrice <maebrie2017@gmail.com>
2023-12-27 20:07:20 +0100

    local test success

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 const corsOptions = {
-  origin: "https://uner.vercel.app/",
+  origin: ["https://uner.vercel.app/", "http://localhost:5173"],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 //* this key finally "open  my door" since the whole day i struggled to solve the pb of empty body data. this tell express to parse the text/plain to json
