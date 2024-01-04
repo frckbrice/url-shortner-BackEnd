5ab45fc3efe43ad8e0c25347156bf45a01578c43
AVOM  Brice <maebrie2017@gmail.com>
2024-01-04 14:00:19 +0100

    Update index.js
    updating the url allowed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = require("path");
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://url-shortner-frontend-git-feature-header-frckbrice.vercel.app/",
+    "https://uner.vercel.app/",
     "*",
   ],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
